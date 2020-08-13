3b6113427c14305ec6d938b1f58addbc6046b025
Rendell Jay Eyas <tsokoloy@gmail.com>
2020-08-13 17:08:55 +0800

    modify bash theme

--- bash/push_theme_moove.sh
+++ bash/push_theme_moove.sh
@@ -8,11 +8,11 @@ rm -rf /var/www/html/moodle39/theme/moove/*
 echo -e "${GREEN}complete${NC}"
 
 echo -e "${RED}copy files${NC}: /var/www/repository_mdl39/theme/moove/* -> /var/www/html/moodle39/theme/moove/"
-cp -R /var/www/repository_mdl39/theme/moove/* /var/www/html/moodle39/theme/moove/
+cp -R /var/www/repository_mdl39/theme/moove/* /var/www/html/moodle39/theme/moove
 
 echo -e "${GREEN}complete${NC}"
 
 echo -e "${RED}changing permission${NC}: /var/www/html/moodle39/theme/moove -> www-data:www-data"
-chown -R www-data:www-data /var/www/html/moodle39/theme/moove
+sudo chown -R www-data:www-data /var/www/html/moodle39/theme/moove
 
 echo -e "${GREEN}done${NC}"
